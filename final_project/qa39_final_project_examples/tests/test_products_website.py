487ad1fac7dbd4b3fb02cee7050281e328f51777
Eldar <eldarba@gmail.com>
2025-06-16 20:31:59 +0300

    auto commit batch

--- final_project/qa39_final_project_examples/tests/test_products_website.py
+++ final_project/qa39_final_project_examples/tests/test_products_website.py
@@ -23,6 +23,7 @@ class TestProductsWebsite(unittest.TestCase):
         self.driver.get(self.BASE_URL)
 
     def test1_add_products(self):
+        time.sleep(1)
         input_product_name = self.driver.find_element(By.ID, 'name')
         input_product_price = self.driver.find_element(By.ID, 'price')
         input_product_image_url = self.driver.find_element(By.ID, 'image')
@@ -30,5 +31,16 @@ class TestProductsWebsite(unittest.TestCase):
         input_product_name.send_keys('Table')
         input_product_price.send_keys('500')
         input_product_image_url.send_keys('table.jpg')
+
+        time.sleep(1)
+        bt_add = self.driver.find_element(By.XPATH, '//*[@id="product-form"]/button/span[1]')
+        bt_add.click()
+        time.sleep(1)
+
+        # test that product name is Table
+        td_table = self.driver.find_element(By.XPATH, '//*[@id="product-table"]/tr/td[2]')
+        self.assertEqual('Table', td_table.text)
+
+        
 
 
