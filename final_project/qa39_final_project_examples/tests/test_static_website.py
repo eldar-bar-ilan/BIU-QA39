695ab00452284bc5c9ee7789f39edd92158458e5
Eldar <eldarba@gmail.com>
2025-06-16 19:08:10 +0300

    auto commit batch

--- final_project/qa39_final_project_examples/tests/test_static_website.py
+++ final_project/qa39_final_project_examples/tests/test_static_website.py
@@ -1,23 +1,29 @@
+import unittest
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 import time
 
-driver = Chrome()
-driver.maximize_window()
-driver.get('http://127.0.0.1:5000/other/home')
+class TestDynamicWebsite(unittest.TestCase):
 
-# click this button to get a prompt popup window
-driver.find_element(By.XPATH, '/html/body/section/main/button[1]').click()
-time.sleep(2)
+    BASE_URL = 'http://127.0.0.1:5000/other/home'
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = Chrome()
+        cls.driver.maximize_window()
 
-# switch the focus to the popup window
-prompt = driver.switch_to.alert
-# send text to the prompt
-prompt.send_keys('This is cool!')
-# click ok to approve
-prompt.accept()
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(3)
+        cls.driver.close()
+
+    def setUp(self):
+        self.driver.get(self.BASE_URL)
+
+    def test1_title(self):
+        title = self.driver.title
+        self.assertEqual('Automation Project', title)
 
 
-time.sleep(10)
-driver.close()
+
+
 
