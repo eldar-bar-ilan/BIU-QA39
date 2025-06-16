76cd7c2cf58e4eb85874bb2234045aca4ab50c2a
Eldar <eldarba@gmail.com>
2025-06-16 19:45:23 +0300

    auto commit batch

--- final_project/qa39_final_project_examples/tests/test_static_website.py
+++ final_project/qa39_final_project_examples/tests/test_static_website.py
@@ -3,9 +3,10 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 import time
 
-class TestDynamicWebsite(unittest.TestCase):
 
+class TestDynamicWebsite(unittest.TestCase):
     BASE_URL = 'http://127.0.0.1:5000/other/home'
+
     @classmethod
     def setUpClass(cls):
         cls.driver = Chrome()
@@ -27,17 +28,29 @@ class TestDynamicWebsite(unittest.TestCase):
         h1_element = self.driver.find_element(By.TAG_NAME, 'h1')
         self.assertEqual('Automation Project - Main Page', h1_element.text)
 
-
     def test3_only_one_h1(self):
         list_h1 = self.driver.find_elements(By.TAG_NAME, 'h1')
         number_of_h1 = len(list_h1)
         self.assertEqual(1, number_of_h1)
 
-
-
     def test4_number_of_tables(self):
         list_tables = self.driver.find_elements(By.TAG_NAME, 'table')
         number_of_tables = len(list_tables)
         self.assertEqual(2, number_of_tables, 'Wrong number of tables')
+
+    def test5_table1_heading(self):
+        table1_heading = self.driver.find_element(By.XPATH, '/html/body/section/main/h2[1]')
+        self.assertEqual('Cities of the World', table1_heading.text)
+
+    def test6_table2_heading(self):
+        table2_heading = self.driver.find_element(By.XPATH, '/html/body/section/main/h2[2]')
+        self.assertEqual('Student Details', table2_heading.text)
+
+    def test7_rows_in_students_table(self):
+        t_body = self.driver.find_element(By.XPATH, '/html/body/section/main/div[2]/table/tbody')
+        row_list = t_body.find_elements(By.TAG_NAME, 'tr')
+        row_count = len(row_list)
+        self.assertEqual(5, row_count, 'Wrong number of rows in students table')
+
 
 
